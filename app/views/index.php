62611adeb38301815309f73459fdfee9fa6a831f
Aashish Nayak <83453974+aashish-nayak@users.noreply.github.com>
2022-02-11 13:43:01 +0530

    PHP MVC Framework Core
    Custom MVC Framework of PHP for Small Projects

---
+++ app/views/index.php
@@ -0,0 +1,11 @@
+<?php
+/* This is how you echo out database information on the screen
+foreach ($data['users'] as $user) {
+    echo "Information: " . $user->user_name . $user->user_email;
+    echo "<br>";
+}
+*/
+?>
+
+<?=$data["title"]?><br>
+HOME PAGE
